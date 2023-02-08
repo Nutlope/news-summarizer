e42d5393b61228bf193becdabdd1f30ed36ddaa6
Denzell Ford <fdenzell@gmail.com>
2023-02-07 22:02:53 -0600

    Added playwright for webscrapping hackernoon webiste

---
+++ pages/api/playwright.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import playwright from "playwright";
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+  const { url } = req;
+  console.log("Received request for", url, "processing");
+  const browser = await playwright.chromium.launch({
+    headless: true // setting this to true will not run the ui
+  });
+
+  if (!url) {
+    res.status(400).json({ error: "no url provided" });
+    return;
+  }
+  let slug = url.split(".com")[1];
+
+  try {
+
+    const page = await browser.newPage();
+    await page.goto(`https://terminal.hackernoon.com${slug}`);
+
+    await page.locator('pre').waitFor();
+
+    let text = await page.$eval('pre', pretag => {
+      return pretag.innerText;
+    });
+
+    await browser.close();
+
+    text = text
+      .replace(/(\r\n|\n|\r)/gm, "")
+      .replace(/(\r\t|\t|\r)/gm, "");
+
+    res.status(200).json({ text });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error });
+  }
+
+
+}
--- pages/api/summarize.ts
+++ pages/api/summarize.ts
@@ -4,12 +4,14 @@ import { OpenAIStream } from "../../utils/OpenAIStream";
 export const config = {
   runtime: "edge",
 };
+const hostname = process.env.HOST;
 
 if (!process.env.OPENAI_API_KEY) {
   throw new Error("Missing env var from OpenAI");
 }
 
 export default async function handler(req: Request) {
+
   const { url } = (await req.json()) as {
     url?: string;
   };
@@ -19,23 +21,35 @@ export default async function handler(req: Request) {
   }
 
   try {
-    const response = await fetch(url, {
-      method: "GET",
-    });
+    let article: string;
 
-    const data = await response.text();
+    if (url.includes("techcrunch")) {
+      const response = await fetch(url, {
+        method: "GET",
+      });
+      const data = await response.text();
 
-    const root = parse(data);
-    const body = root.querySelector(".article-content");
-    const text = body!.innerText
-      .replace(/(\r\n|\n|\r)/gm, "")
-      .replace(/(\r\t|\t|\r)/gm, "");
+      const root = parse(data);
+      const body = root.querySelector(".article-content");
+      article = body!.innerText
+        .replace(/(\r\n|\n|\r)/gm, "")
+        .replace(/(\r\t|\t|\r)/gm, "");
+    } else if (url.includes("hackernoon")) {
+      const response = await fetch(`${hostname}/api/playwright?url=${url}`, {
+        method: "GET",
+      })
+      const data = await response.json();
+      article = (data.text as string);
+    } else {
+      return new Response("Unsupported site", { status: 500 });
+    }
 
-    const prompt = `I want you to act like a news article summarizer. I will input a news article and your job is to convert them into a useful summary of a few sentences. Do not repeat sentences and make sure all sentences are clear and complete: "${text}"`;
+    const pre_prompt = `I want you to act like a news article summarizer. I will input a news article and your job is to convert them into a useful summary of a few sentences. Do not repeat sentences and make sure all sentences are clear and complete:`;
+    const prompt = pre_prompt + article;
 
     const payload = {
-      model: "text-davinci-003",
       prompt,
+      model: "text-davinci-003",
       temperature: 0.7,
       top_p: 1,
       frequency_penalty: 0,
